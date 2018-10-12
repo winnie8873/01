dab74355e12ea2b31e05cd02af7ed36aecf5c489
01 <43664818+winnie8873@users.noreply.github.com>
2018-10-12 15:49:58 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+ window.addEventListener('load', () => {
+ console.log("恭喜你這個拉基終於成功了");
+
+ let x = document.createElement('h1');
+ let m = document.createTextNode(' 這是 <h1> 的文字訊息');
+ let x1 = document.createElement('h2');
+ let m1 = document.createTextNode(' 這是 <h2> 的文字訊息');
+ let x2 = document.createElement('h3');
+ let m2 = document.createTextNode(' 這是 <h3> 的文字訊息');
+ let x3 = document.createElement('h2');
+ let m3 = document.createTextNode(' 這是 <h4> 的文字訊息嗎?');
+ let m4 = document.createTextNode(' 這是 <不知道> 的文字訊息');
+ x.appendChild(m);
+ x1.appendChild(m1);
+ x2.appendChild(m2);
+ x3.appendChild(m3);
+ document.body.appendChild(x);
+ document.body.appendChild(x1);
+ document.body.appendChild(x2);
+ document.body.appendChild(x3);
+ document.body.appendChild(m4);
+ })
